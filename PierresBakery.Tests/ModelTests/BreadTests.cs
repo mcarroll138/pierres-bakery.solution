cd3ca349dc6b1bcf61e4fcb9708d6096dcb34599
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-14 09:55:09 -0700

    good pass BreadOrder test.

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -1,23 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
-using System.Collections.Generic;
-using System;
 
 namespace PierresBakery.Tests
 {
-
-  [TestMethod]
-  public void CalculateTotalCost_ReturnCorrectTotalCost()
+  [TestClass]
+  public class BreadTests
   {
-    //Arrange
-    int numberOfLoaves = 1;
-    int expectedCost = 5;
-    Bread bread = new Bread();
+    [TestMethod]
+    public void CalculateBreadCost_ReturnsCorrectCostForSingleLoaf()
+    {
+      // Arrange
+      int numberOfLoaves = 1;
+      int expectedCost = 5;
+      Bread bread = new Bread();
 
-    //Act
-    int actualCost = bread.CalculateBreadCost(numberOfLoaves);
+      // Act
+      int actualCost = bread.CalculateBreadCost(numberOfLoaves);
 
-    //Assert
-    Assert.AreEqual(expectedCost, actualCost);
+      // Assert
+      Assert.AreEqual(expectedCost, actualCost);
+    }
   }
 }
